3a2c046ed752f353c2feb18e8e02f9efc04b6d60
Dave Robinson <98975807+dave-rob@users.noreply.github.com>
2026-02-26 10:35:25 +0000

    removed leaflet import statement

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Hero from './components/Landing/Hero'
 import CountdownTimer from './components/Landing/Countdown';
 import RecentRuns from './components/Landing/Recent';
 import './App.css';
-import "leaflet/dist/leaflet.css";
 
 function App() {
   const raceDate = '2026-05-17T06:00:00'
