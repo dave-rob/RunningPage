5844ad4be9f7a3515013f66bf9ba93575d8572ae
Dave <dlrobjr@gmail.com>
2026-02-27 04:02:32 -0700

    testing deploy with Render

--- client/src/components/Landing/Recent.jsx
+++ client/src/components/Landing/Recent.jsx
@@ -667,7 +667,8 @@ const RecentRuns = () => {
   const [runs, setRuns] = useState([]);
   useEffect(() => {
   const fetchActivities = async () => {
-    const response = await fetch("http://localhost:5000/api/activities");
+    // const response = await fetch("http://localhost:5000/api/activities");
+    const response = fetch(import.meta.env.VITE_API_URL + "/api/activities")
     const activities = await response.json();
 
     const onlyRuns = activities.filter(
